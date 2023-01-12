28d8011c0c68ccf3809c91c5f30020e5acab9e18
busraozata <busraozata1998@gmail.com>
2023-01-12 18:33:41 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -106,7 +106,32 @@ var swiper = new Swiper(".mySwiperServices", {
     },
   },
 });
+var swiper = new Swiper(".mySwiperDoctors", {
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1600: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    
+  },
+});
 var swiper = new Swiper(".mySwiperGallery", {
   slidesPerView: 3,
   spaceBetween: 30,
